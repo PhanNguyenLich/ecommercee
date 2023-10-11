d28ec8995e8bc92ef34827aa02b17ea090489508
PhanNguyenLich <phanlich408@gmail.com>
2023-10-11 08:03:39 +0700

    update homeScreecn

---
+++ components/ProductItem.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { Pressable } from "react-native";
+
+const ProductItem = ({ item }) => {
+  return (
+    <Pressable style={{ marginHorizontal: 20, marginVertical: 25 }}>
+      <Image
+        style={{ width: 150, height: 150, resizeMode: "contain" }}
+        source={{ uri: item?.image }}
+      />
+      <Text numberOfLines={1} style={{ width: 150, marginTop: 10 }}>
+        {item?.title}
+      </Text>
+
+      <View
+        style={{
+          marginTop: 5,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text style={{ fontSize: 15, fontWeight: "bold" }}>{item?.price}</Text>
+        <Text style={{ color: "#FFC72C", fontWeight: "bold" }}>
+          {item?.rating?.rate} ratings
+        </Text>
+      </View>
+      <Pressable
+        style={{
+          backgroundColor: "#ffc72c",
+          padding: 10,
+          borderRadius: 20,
+          alignItems: "center",
+          justifyContent: "center",
+          marginHorizontal: 10,
+          marginTop: 10,
+        }}
+      >
+        <Text>Add to Cart</Text>
+      </Pressable>
+    </Pressable>
+  );
+};
+
+export default ProductItem;
+
+const styles = StyleSheet.create({});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,11 +9,14 @@ import {
   TextInput,
   Image,
 } from "react-native";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { AntDesign } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 import { SliderBox } from "react-native-image-slider-box";
+import axios from "axios";
+import ProductItem from "../components/ProductItem";
+import DropDownPicker from "react-native-dropdown-picker";
 
 const HomeScreen = () => {
   const list = [
@@ -185,6 +188,29 @@ const HomeScreen = () => {
       size: "8GB RAM, 128GB Storage",
     },
   ];
+  const [products, setProducts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [category, setCategory] = useState("jewelery");
+  const [items, setItems] = useState([
+    { label: "Men's clothing", value: "men's clothing" },
+    { label: "jewelery", value: "jewelery" },
+    { label: "electronics", value: "electronics" },
+    { label: "Women's clothing", value: "women's clothing" },
+  ]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        setProducts(response.data);
+      } catch (error) {
+        console.log("error message", error);
+      }
+    };
+    fetchData();
+  }, []);
+  const onGenderOpen = useCallback(() => {
+    setCompanyOpen(false);
+  }, []);
   return (
     <SafeAreaView
       style={{
@@ -278,6 +304,130 @@ const HomeScreen = () => {
           inactiveDotColor="#90A4AE"
           ImageComponentStyle={{ width: "100%" }}
         />
+
+        <Text style={{ padding: 10, fontSize: 18, fontWeight: "bold" }}>
+          Best Seller
+        </Text>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            flexWrap: "wrap",
+          }}
+        >
+          {deals.map((item, index) => (
+            <Pressable
+              style={{
+                marginVertical: 10,
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                style={{ width: 180, height: 180, resizeMode: "contain" }}
+                source={{ uri: item?.image }}
+              />
+            </Pressable>
+          ))}
+        </View>
+        <Text
+          style={{
+            height: 1,
+            bordeColor: "#D0D0D0",
+            borderWidth: 2,
+            marginTop: 15,
+          }}
+        />
+        <Text style={{ padding: 10, fontSize: 18, fontWeight: "bold" }}>
+          Today's Deals
+        </Text>
+        <ScrollView horizontal showsHorizontalScrollIndicator={true}>
+          {offers.map((item, index) => (
+            <Pressable
+              style={{
+                marginVertical: 10,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Image
+                style={{ width: 150, height: 150, resizeMode: "contain" }}
+                source={{ uri: item?.image }}
+              />
+              <View
+                style={{
+                  backgroundColor: "#E31837",
+                  paddingVertical: 5,
+                  width: 130,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginTop: 10,
+                  borderRadius: 5,
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    color: "white",
+                    fontSize: 13,
+                    fontWeight: "bold",
+                  }}
+                >
+                  {item?.offer}
+                </Text>
+              </View>
+            </Pressable>
+          ))}
+        </ScrollView>
+
+        <Text
+          style={{
+            height: 1,
+            bordeColor: "#D0D0D0",
+            borderWidth: 2,
+            marginTop: 15,
+          }}
+        />
+
+        <View
+          style={{
+            marginHorizontal: 10,
+            marginTop: 20,
+            width: "45%",
+            marginBottom: open ? 50 : 15,
+          }}
+        >
+          <DropDownPicker
+            style={{
+              borderColor: "#B7B7B7",
+              height: 30,
+              marginBottom: open ? 120 : 15,
+            }}
+            open={open}
+            value={category} //genderValue
+            items={items}
+            setOpen={setOpen}
+            setValue={setCategory}
+            setItems={setItems}
+            placeholder="choose category"
+            placeholderStyle={styles.placeholderStyles}
+            onOpen={onGenderOpen}
+            // onChangeValue={onChange}
+            zIndex={3000}
+            zIndexInverse={1000}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            flexWrap: "wrap",
+          }}
+        >
+          {products?.map((item, index) => (
+            <ProductItem item={item} key={index} />
+          ))}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
